3b0cbf88081314e9faea041638e7fcdd062fec9d
Eliel Gomes de Souza da Silva <elielgomessouzas@gmail.com>
2026-09-06 18:28:26 -0300

    Update EX05.java

--- EX05.java
+++ EX05.java
@@ -6,6 +6,9 @@ import javax.swing.JOptionPane;
             a = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do coeficiente A: "));
             b = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do coeficiente B: "));
             c = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor do coeficiente C: "));
-            delta = 
+            delta = (int) Math.pow(b,2) - 4*a*c;
+            raiz1 = (-b + delta)/(2*a); 
+            raiz2 = (-b - (int) Math.sqrt(delta))/(2*a);
+            System.out.println("As duas raizes reais sao: " + raiz1 + raiz2);
         }
 }
