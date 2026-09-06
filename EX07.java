69c915e4032233f4d7afd647555895299c8259ee
Eliel Gomes de Souza da Silva <elielgomessouzas@gmail.com>
2026-09-06 18:45:37 -0300

    Update EX07.java

--- EX07.java
+++ EX07.java
@@ -2,11 +2,11 @@ import javax.swing.JOptionPane;
 
     public class EX07 {
         public static void main (String args[]) {
-            double altura, base, comprimento, area;
-            altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do paralelepípedo: "));
-            base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base do paralelepípedo: "));
-            comprimento = Double.parseDouble(JOptionPane.showInputDialog("Digite o comprimento do paralelepípedo: "));
-            area = (altura * base)*2 + (base * comprimento)*2 + (altura * comprimento)*2;
-            JOptionPane.showMessageDialog(null,"A área do paralelepípedo é: " + area);
+            int altura, largura, comprimento, area;
+            altura = Integer.parseInt(JOptionPane.showInputDialog("Digite a altura do paralelepípedo: "));
+            largura = Integer.parseInt(JOptionPane.showInputDialog("Digite a largura do paralelepípedo: "));
+            comprimento = Integer.parseInt(JOptionPane.showInputDialog("Digite o comprimento do paralelepípedo: "));
+            area = 2*((altura*largura) + (altura*comprimento) + (largura*comprimento));
+            System.out.println("A área do paralelepípedo é: " + area);
         }
 }
