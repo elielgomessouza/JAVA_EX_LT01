5ccf991b49f403f7fb66d7b784232a21a64d4485
Eliel Gomes de Souza da Silva <elielgomessouzas@gmail.com>
2026-09-06 18:26:58 -0300

    Update EX09.java

--- EX09.java
+++ EX09.java
@@ -5,8 +5,7 @@ import javax.swing.JOptionPane;
             int x, y, z;
             x = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de x: "));
             y = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor de y: "));
-            z = Math.pow(x, 2);
-            JOptionPane.showInputDialog(null, "O valor dos quadrados são: " x + y);
-            
+            z = (int) Math.pow(x, 2) + (int) Math.pow(y, 2);
+            System.out.println("O valor dos quadrados são: " + z);
         }
 }
